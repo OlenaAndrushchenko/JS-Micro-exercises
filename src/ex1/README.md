82f54f140e64d42f0cf6170acf69ffb0cd52ecfc
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-05-28 09:23:03 +0200

    ex1 done

---
+++ src/ex1/README.md
@@ -0,0 +1,4 @@
+# Ex1
+
+## Task
+Write a program that writes a text of type <h1> on the screen that says “Hello Javascript”.
---
+++ src/ex1/ex1.js
@@ -0,0 +1,14 @@
+// Given - I am on the html page
+// When the page is rendered
+// Then I see the title Hello Javascript
+
+const title = "Hello Javascript"
+const container = document.getElementById("h1")
+
+function renderTitle(title) {
+    container.innerHTML = /* html */ `<h1>${title}</h1>`
+}
+
+renderTitle(title)
+
+console.log(container)
