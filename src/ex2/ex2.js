010f696e320ef04bf7443def91b25dd5ba3a61b6
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-05-29 01:14:28 +0200

    added another approach to ex2.js, created files for ex3

--- src/ex2/ex2.js
+++ src/ex2/ex2.js
@@ -2,15 +2,33 @@
 // When the page is rendered
 // Then it shows the text "The sum of 3 + 5 is 8"
 
+
 const numb1 = 3
 const numb2 = 5
-const total = numb1 + numb2
-const text = `The sum of ${numb1} + ${numb2} is ${total}` 
-// console.log(text);
 
-function render() {     
-    const container = document.getElementById('container')
-    container.innerHTML = text 
+// function render() {     
+//     const container = document.getElementById('container')
+//     const total = numb1 + numb2
+//     const text = `The sum of ${numb1} + ${numb2} is ${total}` 
+//     container.innerHTML = text 
+// }
+
+// render()
+
+// using 2 functions
+function sum(parameter1, parameter2) {
+    let total = parameter1 + parameter2
+    console.log('p1', parameter1);
+    console.log('p2', parameter2);
+    console.log('total', total);
+    return total
 }
 
-render()
+function render(parameter1, parameter2, total) {
+    const container = document.getElementById('container')
+    const text = `The sum of ${parameter1} + ${parameter2} is ${total}`
+    container.innerHTML = text
+    console.log('render')
+}
+
+render(numb1, numb2, sum(numb1, numb2))
---
+++ src/ex3/README.md
@@ -0,0 +1,4 @@
+# Ex3
+
+## Task
+Write a program that asks for the user's name with an input and writes a text that says “Hello <user-name>”
