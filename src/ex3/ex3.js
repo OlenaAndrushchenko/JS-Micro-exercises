e11adb9b42b4036c71fa4bb7170bef7f203d9fea
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-05-29 04:31:21 +0200

    comments for ex3

---
+++ src/ex3/ex3.js
@@ -0,0 +1,8 @@
+// Given user on the page with form
+// When user fills the form and click submit button
+// Then below the form appears text "Hello <username>"
+
+
+// take username.value
+// render html container with text "hello <username.value>"
+
