604a0ce4c4e593b2c19104ba00df85a858c3c6e1
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-29 00:21:16 +0200

    code for ex5

---
+++ src/ex-5/README.md
@@ -0,0 +1,5 @@
+# Ex5
+
+## Task
+
+Write a program that asks for two numbers and writes on the screen which one is the largest.
--- src/ex-5/ex-5.js
+++ src/ex-5/ex-5.js
@@ -1,8 +1,8 @@
 // Given user on the page with form
 // When user fills the form and click submit button
-// Then below the form appears text “The sum of <number-one> with <number-two> is <result>”
+// Then below the form appears text “The largest number is <result>”
 
-// add event listener to form -> take parsed to int values from inputs -> result that sum numbers -> print text in html
+// add event listener to form -> take parsed to int values from inputs -> check which num is largest -> print text in html
 
 function render() {
     
@@ -10,9 +10,11 @@ function render() {
         e.preventDefault();
         const num1 = parseInt(document.getElementById('num-one').value);
         const num2 = parseInt(document.getElementById('num-two').value);
-        const result = num1 + num2;
+
+        const result = Math.max(num1, num2);
+
         const container = document.getElementById('container');
-        const text = `The sum of ${num1} + ${num2} is ${result}`
+        const text = `The largest number is ${result}`
         container.innerHTML = text
 
     })
