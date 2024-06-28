9987fcb14cc4484bcdac7551e9dd79ea05d852af
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-28 23:50:01 +0200

    code for ex4

--- src/ex-4/ex-4.js
+++ src/ex-4/ex-4.js
@@ -1,7 +1,15 @@
-// function render() {
-//     const container = document.getElementById('container')
-//     const text = `The sum of ${num1} + ${num2} is ${total}`
-//     container.innerHTML = text
-// }
+function render() {
+    
+    document.getElementById('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const num1 = parseInt(document.getElementById('num-one').value);
+        const num2 = parseInt(document.getElementById('num-two').value);
+        const result = num1 + num2;
+        const container = document.getElementById('container');
+        const text = `The sum of ${num1} + ${num2} is ${result}`
+        container.innerHTML = text
 
-// render()
+    })
+}
+
+render()
